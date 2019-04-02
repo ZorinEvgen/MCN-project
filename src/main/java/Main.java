1622ab9d578c55e4297d307af660722e12a65e13
Zorin Evgeniy <evgeniy.zorin@yahoo.com>
2019-04-02 22:04:38 +0300

    Completed first part of the DB (real estate related)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         List<EntityService> services = new ArrayList<EntityService>();
 
+
         services.add(new EstateTypeService());
         services.add(new FederalEntityService());
         services.add(new DistrictService());
